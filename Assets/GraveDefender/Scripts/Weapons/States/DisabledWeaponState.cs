3bd43421292094ce5f0eff631383f3fb0b4491ba
Chris <crohr00@gmail.com>
2014-11-19 19:21:03 -0600

    Separate WeaponStates

---
+++ Assets/GraveDefender/Scripts/Weapons/States/DisabledWeaponState.cs
@@ -0,0 +1,45 @@
+﻿
+
+public class DisableWeaponState : WeaponState
+{
+    private Weapon _weapon;
+    private float _timeToFireNext;
+    
+    public int CurrentAmmo { get; set; }
+    
+    public DisableWeaponState(Weapon weapon)
+    {
+        _weapon = weapon;
+    }
+    
+    public void Ready()
+    {
+    }
+    
+    public void Use()
+    {
+    }
+    
+    public void Reload()
+    {
+    }
+    
+    public void Disable()
+    {
+    }
+    
+    public WeaponState SwitchToReadyState()
+    {
+        return new ReadyWeaponState(_weapon);
+    }
+    
+    public WeaponState SwitchToReloadState()
+    {
+        return new ReloadWeaponState(_weapon);
+    }
+    
+    public WeaponState SwitchToDisableState()
+    {
+        return this;
+    }
+}
---
+++ Assets/GraveDefender/Scripts/Weapons/States/ReloadWeaponState.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReloadWeaponState : WeaponState
+{
+    private Weapon _weapon;
+    private float _timeToFireNext;
+    
+    public int CurrentAmmo { get; set; }
+    
+    public ReloadWeaponState(Weapon weapon)
+    {
+        _weapon = weapon;
+    }
+    
+    public void Ready()
+    {
+    }
+    
+    public void Use()
+    {
+    }
+    
+    public void Reload()
+    {
+        GameManager.Instance.StartCoroutine(ReloadRoutine());
+    }
+    
+    private IEnumerator ReloadRoutine()
+    {
+        yield return new WaitForSeconds(Mathf.Max(0f, _weapon.ReloadTime.ModifiedValue));
+        _weapon.Ready();
+    }
+    
+    public void Disable()
+    {
+    }
+    
+    public WeaponState SwitchToReadyState()
+    {
+        return new ReadyWeaponState(_weapon);
+    }
+    
+    public WeaponState SwitchToReloadState()
+    {
+        return this;
+    }
+    
+    public WeaponState SwitchToDisableState()
+    {
+        return new DisableWeaponState(_weapon);
+    }
+}
