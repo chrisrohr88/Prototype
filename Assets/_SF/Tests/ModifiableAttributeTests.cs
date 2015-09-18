4285accbe1417a7e107d809d074df39bdf46e2ad
Chris <crohr00@gmail.com>
2015-09-18 00:47:54 -0500

    New Test dlls

--- Assets/_SF/Tests/ModifiableAttributeTests.cs
+++ Assets/_SF/Tests/ModifiableAttributeTests.cs
@@ -2,6 +2,7 @@ using SFUnitTest;
 using UnityEngine;
 using System.Collections;
 
+[TestClass()]
 public class ModifiableAttributeTests : Test
 {
 	private ModifiableAttribute _attribute;
@@ -12,21 +13,31 @@ public class ModifiableAttributeTests : Test
 		Debug.Log("Setup!!!");
 	}
 
+	[Test()]
 	public void CheckBaseValue()
 	{
 		Assert.AssertEquals(100, _attribute.ModifiedValue);
 	}
-	
+
+	[Test()]
 	public void AddModifierTest()
 	{
 		_attribute.AddModifierAndUpdateAttribute(1.1f);
 		Assert.AssertEquals(110, _attribute.ModifiedValue);
 	}
-	
+
+	[Test()]
 	public IEnumerator ModifierExpiredTest()
 	{
 		_attribute.AddModifierAndUpdateAttribute(1.1f, 1);
 		yield return new WaitForSeconds(2);
 		Assert.AssertEquals(100, _attribute.ModifiedValue);
+	}
+
+	[Test()]
+	public IEnumerator LastTest()
+	{
+		var go = (GameObject.Instantiate(Resources.Load("Test")) as GameObject).GetComponent<TestFoTests>();
+		yield return StartCoroutine(go.Test2());
 	}
 }
