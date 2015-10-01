118bfb7786d79465d985dfb01f1dda44e8cddefa
Chris <crohr00@gmail.com>
2015-09-30 23:24:43 -0500

    UnitTestTest

---
+++ Assets/_SF/Tests/TestFoTests.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestFoTests : MonoBehaviour
+{
+	public IEnumerator Test1()
+	{
+		yield return StartCoroutine(Test3());
+		Debug.Log("Done in Test1");
+	}
+
+	public IEnumerator Test2()
+	{
+		yield return StartCoroutine(Test1());
+		Debug.Log("Done in Test2");
+	}
+
+	public IEnumerator Test3()
+	{
+		yield return new WaitForSeconds(1);
+		Debug.Log("Done in Test3");
+
+	}
+	public IEnumerator Test4()
+	{
+		yield return StartCoroutine(Test5());
+		Debug.Log("Done in Test3");
+	}
+
+	public IEnumerator Test5()
+	{
+		yield return new WaitForSeconds(1);
+		throw new System.Exception("Error!");
+	}
+}
