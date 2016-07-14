11480663b79b287187bafea3481e8145234c56bb
Chris <crohr00@gmail.com>
2016-07-13 20:43:49 -0500

    Test commit

--- Assets/Rotation.cs
+++ Assets/Rotation.cs
@@ -5,6 +5,7 @@ public class Rotation : MonoBehaviour
 {
 	[SerializeField] private float _speed = 5;
 
+	//Test
 	public void Update()
 	{
 		transform.RotateAround(transform.position, new Vector3(0, 0, 1), Time.deltaTime * _speed);
